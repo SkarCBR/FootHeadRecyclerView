b2f05ae55f750fb3c5269bc863c2963446f8bdae
Oscar Olivella <oscar.olivella@scrm.eu>
2020-06-02 13:51:41 +0200

    match menu id with layout xml ids for navigation

--- app/src/main/java/com/mrskar/samples/presentation/view/MainActivity.kt
+++ app/src/main/java/com/mrskar/samples/presentation/view/MainActivity.kt
@@ -2,59 +2,36 @@ package com.mrskar.samples.presentation.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mrskar.samples.R
 import com.mrskar.samples.presentation.vm.MainViewModel
 import com.mrskar.samples.presentation.vm.getViewModelInstance
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-
-    private val recyclerFragment =
-        RecyclerViewFragment()
-    private val mapFragment = MapsFragment()
-    private val fragmentThree = FragmentThree()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewModel = getViewModelInstance(MainViewModel::class.java)
-        initView()
-    }
 
-    private fun initView() {
-        replaceFragment(recyclerFragment)
-        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.app_bar_recyclerview -> {
-                    replaceFragment(recyclerFragment)
-                    true
-                }
-                R.id.app_bar_map -> {
-                    replaceFragment(mapFragment)
-                    true
-                }
-                R.id.app_bar_search -> {
-                    replaceFragment(fragmentThree)
-                    true
-                }
-                else -> {
-                    for (fragment in supportFragmentManager.fragments) {
-                        supportFragmentManager.beginTransaction().remove(fragment).commit()
-                    }
-                    true
-                }
-            }
+        if (savedInstanceState == null) {
+            setupNavController()
         }
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.main_container, fragment)
-            .commit()
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupNavController()
+    }
+
+    private fun setupNavController() {
+        val navController = findNavController(R.id.main_nav_host_fragment)
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+            .setupWithNavController(navController)
     }
 }
