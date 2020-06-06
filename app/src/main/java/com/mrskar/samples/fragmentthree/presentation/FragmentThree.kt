2bc3188ccb17fb86bdcba85c6222efeff51cf81b
Oscar Olivella Garcia <oolivella@gmail.com>
2020-06-06 18:09:47 +0200

    split code and fix fragment id reference

--- app/src/main/java/com/mrskar/samples/fragmentthree/presentation/FragmentThree.kt
+++ app/src/main/java/com/mrskar/samples/fragmentthree/presentation/FragmentThree.kt
@@ -25,15 +25,6 @@ import kotlinx.android.synthetic.main.bottom_sheet_layout.*
 class FragmentThree : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         setUpMap(googleMap)
     }
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
@@ -48,9 +39,14 @@ class FragmentThree : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.fragment_three_map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
 
+        setUpBottomSheetLayout()
+    }
+
+    private fun setUpBottomSheetLayout() {
         bottomSheetBehavior =
             BottomSheetBehavior.from(bottom_sheet_container).apply {
                 state = STATE_HIDDEN
@@ -80,19 +76,35 @@ class FragmentThree : Fragment() {
 
     private fun showBottomSheetView() {
         when (bottomSheetBehavior.state) {
-            STATE_EXPANDED -> { bottomSheetBehavior.state = STATE_COLLAPSED }
-            STATE_COLLAPSED -> { bottomSheetBehavior.state = STATE_EXPANDED }
-            STATE_HIDDEN -> { bottomSheetBehavior.state = STATE_EXPANDED}
+            STATE_EXPANDED -> {
+                bottomSheetBehavior.state = STATE_COLLAPSED
+            }
+            STATE_COLLAPSED -> {
+                bottomSheetBehavior.state = STATE_EXPANDED
+            }
+            STATE_HIDDEN -> {
+                bottomSheetBehavior.state = STATE_EXPANDED
+            }
         }
     }
 
     private fun handleBottomSheetState() {
         when (bottomSheetBehavior.state) {
-            STATE_EXPANDED -> { Log.d("BottomSheet State: ","State Expanded") }
-            STATE_COLLAPSED -> { Log.d("BottomSheet State: ","State Collapsed") }
-            STATE_HALF_EXPANDED -> { Log.d("BottomSheet State: ","State Half Expanded") }
-            STATE_DRAGGING -> { Log.d("BottomSheet State: ","State Dragging") }
-            STATE_HIDDEN -> { Log.d("BottomSheet State: ","State Hidden") }
+            STATE_EXPANDED -> {
+                Log.d("BottomSheet State: ", "State Expanded")
+            }
+            STATE_COLLAPSED -> {
+                Log.d("BottomSheet State: ", "State Collapsed")
+            }
+            STATE_HALF_EXPANDED -> {
+                Log.d("BottomSheet State: ", "State Half Expanded")
+            }
+            STATE_DRAGGING -> {
+                Log.d("BottomSheet State: ", "State Dragging")
+            }
+            STATE_HIDDEN -> {
+                Log.d("BottomSheet State: ", "State Hidden")
+            }
         }
     }
 }
