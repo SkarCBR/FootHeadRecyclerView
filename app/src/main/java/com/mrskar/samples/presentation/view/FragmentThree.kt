204117b63922e1a6cccc7a7836820f65e481bb0f
Oscar Olivella Garcia <oolivella@gmail.com>
2020-06-06 13:43:41 +0200

    initial bottom sheet implementation on fragment three

--- app/src/main/java/com/mrskar/samples/presentation/view/FragmentThree.kt
+++ app/src/main/java/com/mrskar/samples/presentation/view/FragmentThree.kt
@@ -1,6 +1,7 @@
 package com.mrskar.samples.presentation.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,8 +12,14 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_COLLAPSED
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_DRAGGING
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_EXPANDED
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HALF_EXPANDED
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HIDDEN
 import com.mrskar.samples.R
-import com.mrskar.samples.presentation.view.components.BottomSheetFragment
+import kotlinx.android.synthetic.main.bottom_sheet_layout.*
 
 class FragmentThree : Fragment() {
 
@@ -26,8 +33,9 @@ class FragmentThree : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        setInitialPosition(googleMap)
+        setUpMap(googleMap)
     }
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,20 +49,46 @@ class FragmentThree : Fragment() {
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet_container)
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                handleBottomSheetState()
+            }
+
+        })
     }
 
-    private fun setInitialPosition(googleMap: GoogleMap) {
+    private fun setUpMap(googleMap: GoogleMap) {
         val home = LatLng(41.3972851, 2.1276549)
         googleMap.addMarker(MarkerOptions().position(home).title("Home"))
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(home, 12F))
         googleMap.setOnMarkerClickListener {
-            showBottomSheetFragment()
+            showBottomSheetView()
             true
         }
     }
 
-    private fun showBottomSheetFragment() {
-        val bottomSheetFragment = BottomSheetFragment()
-        bottomSheetFragment.show(childFragmentManager, bottomSheetFragment.tag)
+    private fun showBottomSheetView() {
+        when (bottomSheetBehavior.state) {
+            STATE_EXPANDED -> { bottomSheetBehavior.state = STATE_COLLAPSED }
+            STATE_COLLAPSED -> { bottomSheetBehavior.state = STATE_EXPANDED }
+            STATE_HIDDEN -> { bottomSheetBehavior.state = STATE_EXPANDED}
+        }
+    }
+
+    private fun handleBottomSheetState() {
+        when (bottomSheetBehavior.state) {
+            STATE_EXPANDED -> { Log.d("BottomSheet State: ","State Expanded") }
+            STATE_COLLAPSED -> { Log.d("BottomSheet State: ","State Collapsed") }
+            STATE_HALF_EXPANDED -> { Log.d("BottomSheet State: ","State Half Expanded") }
+            STATE_DRAGGING -> { Log.d("BottomSheet State: ","State Dragging") }
+            STATE_HIDDEN -> { Log.d("BottomSheet State: ","State Hidden") }
+        }
     }
 }
